81ced85840c0548aeaf8ad2692a30ec92ca30a3b
magdalenahuget <magdalena.huget@gmail.com>
2023-02-27 22:39:30 +0100

    Refactor names

--- src/components/welcome/WelcomeImg.js
+++ src/components/welcome/WelcomeImg.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import welcomeImg from '../../resources/welcomeImg.jpg';
 
-function WelcomeImgComponent() {
+function WelcomeImg() {
 
     return (<>
         <img className="welcome-img" alt="welcome-img" src={welcomeImg} />
@@ -9,4 +9,4 @@ function WelcomeImgComponent() {
     )
 }
 
-export default WelcomeImgComponent
+export default WelcomeImg
