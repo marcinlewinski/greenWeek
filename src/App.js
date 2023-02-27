55e2e8143c5d765724827b19d95611a8bcb0fb30
magdalenahuget <magdalena.huget@gmail.com>
2023-02-27 22:43:42 +0100

    Add About component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Welcome from './components/welcome/Welcome';
 import Ideas from './components/Ideas';
+import About from './components/about/About';
 
 function App() {
   return (
     <div className="App">
       <Welcome />
       <Ideas />
+      <About />
     </div>
   );
 }
