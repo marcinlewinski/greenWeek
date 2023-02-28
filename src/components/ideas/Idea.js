9b2f28a0d0a779c9ccb9b5969b69f193758bc229
Wiktoria Kulczynska <wiktoriakulczynska@gmail.com>
2023-02-28 14:41:25 +0100

    idea styling

--- src/components/ideas/Idea.js
+++ src/components/ideas/Idea.js
@@ -2,14 +2,10 @@ import "./Idea.css";
 const Idea = ({ img, title, description, handleModal }) => {
   return (
     <div className="idea">
-      <div className="image-wrapper">
-        <img className="idea-img" src={img} alt="{title}" />
-      </div>
-      <div className="text-wrapper">
-        <div className="idea-content">
-          <h1 className="idea-title">{title}</h1>
-          <p className="idea-description">{description}</p>
-        </div>
+      <div className='image-wrapper' style={{backgroundImage: `url(${img})`}}></div>
+      <div className="idea-content">
+        <h1 className="idea-title">{title}</h1>
+        <p className="idea-description">{description}</p>
         <button type="submit" className="idea-btn" onClick={handleModal}>
           Discover more!
         </button>
