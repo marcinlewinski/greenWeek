5ee8f5f5095d99ef6277596d14ebeb0aeb4104d7
Wiktoria Kulczynska <wiktoriakulczynska@gmail.com>
2023-02-28 12:06:32 +0100

    refactor

---
+++ src/components/welcome/WelcomeImg.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function WelcomeImg() {
+
+    return (<>
+        <img className="welcome-img" src="/welcomeImg.jpg" alt="welcome-img"/>
+    </>
+    )
+}
+
+export default WelcomeImg
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.querySelector('body'));
+const root = ReactDOM.createRoot(document.querySelector('#root'));
 root.render(
   <React.StrictMode>
     <App />
