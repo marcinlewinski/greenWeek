d40b82f9cc68c2ad29cd2d129adb9d2a5ff17395
marcinlewinski <marcinlewinski71@gmail.com>
2023-03-01 21:00:20 +0100

    pictures for DB

--- src/database/data.js
+++ src/database/data.js
@@ -4,61 +4,61 @@ const data = [
   {
     title: 'Bring your own reusable bags',
     description: 'When shopping, its easy to bring your own reusable bags instead of relying on single-use plastic bags. Not only is it better for the environment, but it can also save you money in the long run.Reusable bags come in all shapes and sizes, so you can find ones that work for your needs. Some are foldable and compact, making them easy to carry around, while others are sturdier and can hold heavier items.',
-    img: 'https://www.harmony-textile.com/c/445-category_default/bag-pouch.jpg',
-    plasticWaste: '0kg'
+    img: 'reusable-bags-pexels-taryn-elliott-4440171.jpg',
+    plasticWaste: '0.4kg'
   },
   {
     title: 'Use a refillable water bottle',
     description: 'Disposable plastic water bottles are a major source of plastic waste, with millions of them discarded every day. By using a refillable water bottle, you can help reduce this waste and save money. Refillable water bottles come in many different materials, such as stainless steel, glass, or BPA-free plastic. Choosing a high-quality bottle can help ensure that it lasts for a long time and doesnt leak or break easily. Refillable water bottles are also convenient to use, as you can fill them up at home or at a water fountain instead of constantly buying new bottles.',
-    img: 'https://www.indy100.com/media-library/best-reusable-water-bottles-to-help-you-reduce-plastic-use-and-save-money.jpg?id=28064059&width=1245&height=700&quality=85&coordinates=0%2C253%2C0%2C253',
+    img: 'refillable-bottle-pexels-alex-azabache-3766180.jpg',
     plasticWaste: '0.5kg'
   },
   {
     title: 'Avoid individually wrapped items',
     description: 'Individually wrapped items like snack bars or candies may be convenient, but they also create a lot of unnecessary plastic waste. Choosing bulk purchases instead can help reduce this waste and save you money. Many stores offer bulk sections where you can buy items like nuts, dried fruit, or granola in reusable containers or bags. Bringing your own reusable bags or containers can make this even more eco-friendly. When you do need to buy individually wrapped items, look for ones that use eco-friendly packaging materials like paper or compostable plastics.',
-    img: 'https://m.media-amazon.com/images/I/912PGqdk7WL._SX679_.jpg',
+    img: 'individually-wrapped-items-pexels-tim-gouw-443419.jpg',
     plasticWaste: '1kg'
   },
   {
     title: 'Use glass containers or reusable silicone bags',
     description: 'Plastic bags and containers are a major source of plastic waste, with millions of them discarded every day. Choosing glass containers or reusable silicone bags for food storage can help reduce this waste and keep your food fresher for longer..',
-    img: 'https://www.pamperedchef.ca/iceberg/com/product/100223-lg.jpg',
+    img: 'glass-containers-pexels-ella-olsson-1640768.jpg',
     plasticWaste: '1.5kg'
   },
   {
     title: 'Bring your own reusable coffee cup or mug',
     description: 'Alternatively, you can try making your own coffee at home with a French press or drip coffee maker. This not only reduces plastic waste but can also save you money in the long run. Dont forget to bring your reusable cup or mug with you when youre on the go!.',
-    img: 'https://i.etsystatic.com/17675784/r/il/5ae9f7/3207415097/il_fullxfull.3207415097_6ftl.jpg',
+    img: 'coffee-cup-pexels-bruno-cervera-2744760.jpg',
     plasticWaste: '2kg'
   },
   {
     title: 'Choose products with minimal or eco-friendly packaging',
     description: 'Choosing products with minimal or eco-friendly packaging is another great way to reduce plastic waste. Opt for bar soap instead of liquid soap in plastic bottles, or try using shampoo bars instead of bottled shampoo. You can also look for products that are packaged in recyclable or biodegradable materials.',
-    img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT4KVMUC3PsHuFmeFFgSUmtzgnhSiOkonE0XQ&usqp=CAU',
+    img: 'eco-friendly-packaging-pexels-cup-of-couple-6634186.jpg',
     plasticWaste: '2.5kg'
   },
   {
     title: 'Use cloth napkins',
     description: 'Using cloth napkins instead of disposable paper napkins is another simple swap that can make a big difference. Not only are cloth napkins more eco-friendly, but they can also add a touch of elegance to your table setting. You can easily wash and reuse cloth napkins, and they come in a variety of colors and patterns to suit any style.',
-    img: 'https://u9j4i3a9.stackpathcdn.com/wp-content/uploads/2020/10/How-to-Sew-Cloth-Napkins.jpg',
+    img: 'cloth-napkins-pexels-monstera-7114107.jpg',
     plasticWaste: '3kg'
   },
   {
     title: 'Purchase items in bulk',
     description: 'When shopping, try to purchase items in bulk to avoid excess packaging. This can include buying items like rice, pasta, and nuts in bulk bins at the grocery store. Be sure to bring your own reusable bags or containers to fill up with your purchases',
-    img: 'https://www.bulkco.co.uk/wp-content/uploads/2020/07/Popular-Items-to-Buy-in-Bulk.jpg',
+    img: 'bulk-food-pexels-markus-winkler-12244891.jpg',
     plasticWaste: '3.5kg'
   },
   {
     title: 'Choose products made from alternative materials',
     description: 'Choosing products made from alternative materials to plastic is another way to reduce plastic waste. For example, you can try using bamboo utensils instead of plastic ones. Bamboo is a sustainable and biodegradable material that can be used to make a variety of household items, from utensils to cutting boards',
-    img: 'https://www.compliancegate.com/wp-content/uploads/2020/01/wooden-kitchen-utensils-regulations-eu.jpg',
+    img: 'alternative-materials-pexels-karolina-grabowska-4397820.jpg',
     plasticWaste: '4kg'
   },
   {
     title: 'Consider composting food scraps',
     description: 'Finally, consider composting food scraps instead of using plastic bags for disposal. Composting not only reduces the amount of waste sent to landfills, but it can also provide nutrient-rich soil for your garden. You can compost a variety of food scraps, including fruit and vegetable scraps, eggshells, and coffee grounds. Just be sure to do some research on the best practices for composting in your area.',
-    img:'https://www.conserve-energy-future.com/wp-content/uploads/2022/08/pouring-food-scraps-in-compost.jpg',
+    img:'composting-food-pexels-eva-bronzini-5503338.jpg',
     plasticWaste: '4.1kg'
 
   },
