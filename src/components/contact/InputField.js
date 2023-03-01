6063dba611c2572ab8ee8000c122206f0e8bdece
marcinlewinski <marcinlewinski71@gmail.com>
2023-03-01 09:45:42 +0100

    corrected

--- src/components/contact/InputField.js
+++ src/components/contact/InputField.js
@@ -2,22 +2,24 @@ function InputField(props) {
 
     const {onBlure, touched,  className,error,onChange,values, key, type, label,  formpopup, workshopTitle } = props;
 
+    const defaultContent = () => {
+        if (formpopup && type === 'subject') {
+          return workshopTitle;
+        }
+        if (formpopup && type === 'message') {
+          return `I'm interested i this Workshop! `;
+        }
+        return values;
+      };
+
     return (
         <>
             <label htmlFor={type}>{label}</label>
             <input className={error[type] && touched ? className : ""} onBlur={onBlure} onChange={onChange}   value={defaultContent()} id={type} key={key} type={type} placeholder={label}></input>
             {error[type] && touched && <p className="error">{error[type]}</p>}
         </>
-
-  const defaultContent = () => {
-    if (formpopup && type === 'subject') {
-      return workshopTitle;
-    }
-    if (formpopup && type === 'message') {
-      return `I'm interested i this Workshop! `;
-    }
-    return values;
-  };
+    )
+}
 
 
 export default InputField;
